ecf082cfdc5e57b6e551bd2d201d172350e97d33
ceveloper7 <cevabarcvilla@gmail.com>
2026-04-14 14:14:20 +0000

    Spring Framework - Chapter 2 - beans stereotypes

--- src/main/java/com/gba/dating4e/Dating4eApplication.java
+++ src/main/java/com/gba/dating4e/Dating4eApplication.java
@@ -1,9 +1,12 @@
 package com.gba.dating4e;
 
+import com.gba.dating4e.core.FileSystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.slf4j.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.util.unit.DataSize;
 
 @SpringBootApplication
 public class Dating4eApplication {
@@ -11,7 +14,10 @@ public class Dating4eApplication {
 	private static final Logger log = LoggerFactory.getLogger(Dating4eApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(Dating4eApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(Dating4eApplication.class, args);
+
+		FileSystem fs = ctx.getBean(FileSystem.class);
+		System.out.println(DataSize.ofBytes(fs.getFreeDiskSpace()).toBytes() + " GB");
 	}
 
 }
--- src/main/java/com/gba/dating4e/core/FileSystem.java
+++ src/main/java/com/gba/dating4e/core/FileSystem.java
@@ -1,4 +1,52 @@
 package com.gba.dating4e.core;
 
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Component for an abstraction of a file System for store picture in file system
+ *
+ */
+@Service
 public class FileSystem {
+    // root directory of logical file system
+    private final Path root = Paths.get(System.getProperty("user.home")).resolve("fs");
+
+    public FileSystem(){
+        try{
+            if (!Files.isDirectory(root)){
+                Files.createDirectory(root);
+            }
+        }catch (IOException e){
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * @return free space
+     */
+    public long getFreeDiskSpace(){
+        return root.toFile().getFreeSpace();
+    }
+
+    public byte[] load(String fileName){
+        try{
+            return Files.readAllBytes(root.resolve(fileName));
+        }catch (IOException e){
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public void store(String fileName, byte[] bytes){
+        try{
+            Files.write(root.resolve(fileName), bytes);
+        }catch (IOException e){
+            throw new UncheckedIOException(e);
+        }
+    }
 }
